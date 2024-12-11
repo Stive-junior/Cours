2ce4162360d0b6089369a04c59441ed68d93fe0d
Stive <nanatchoffojunior@gmail.com>
2024-12-11 01:47:26 +0100

    Déploiement de ICT4D

--- obj/Release/net8.0/ICT4D.AssemblyInfo.cs
+++ obj/Release/net8.0/ICT4D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICT4D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51181aaf1cf5db33b95d423e636c47f7de84c53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICT4D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICT4D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
